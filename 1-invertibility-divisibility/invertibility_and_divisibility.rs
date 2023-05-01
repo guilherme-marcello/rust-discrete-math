106ad887f6edb3d8feb8608f66d3743c13da66c0
guilherme-marcello <guilemosm@gmail.com>
2023-05-01 10:25:45 +0100

    Added function to calculate the multiplicative inverse

--- 1-invertibility-divisibility/invertibility_and_divisibility.rs
+++ 1-invertibility-divisibility/invertibility_and_divisibility.rs
@@ -1,6 +1,7 @@
 #[allow(dead_code)]
 #[path = "../0-euclidian-algorithm/euclid.rs"] mod euclid;
 use euclid::gcd;
+use euclid::ext;
 
 /*
  * Returns true if (a,b) are coprime. false otherwise
@@ -16,8 +17,20 @@ fn coprime(a: i32, b: i32) -> bool {
  * @param `a`       - Value of the modular integer
  * @param `modulus` - Modulus
  */
-fn is_invertible(a: i32, modulus: i32) -> bool {
+pub fn is_invertible(a: i32, modulus: i32) -> bool {
     return coprime(a, modulus);
+}
+
+/*
+ * Returns the positive multiplicative inverse of a in Z_{modulus}.
+ * @requires is_invertible(a, modulus)
+ * @ensures \result > 0
+ * @param `a`       - Value of the modular integer
+ * @param `modulus` - Modulus
+ */
+pub fn inverse(a: i32, modulus: i32) -> i32 {
+    let (x, _, _) = ext(a, modulus);
+    return if x < 0 {x + modulus} else {x};
 }
 
 fn main() {
@@ -26,4 +39,13 @@ fn main() {
         assert_eq!(is_invertible(i, i + 1), true);
         i += 1;
     }
+
+    let a = 3;
+    let n = 11;
+    let a_inverse = inverse(a, n);
+    let neg_a_inverse = a_inverse - n;
+
+    println!("x.{a} ≡ 1 mod {n} <=>");
+    println!("{a_inverse}.{a} ≡ 1 mod {n}");
+    println!("{a_inverse} and {neg_a_inverse} are multiplicative inverses of {a} in Z_{n}");
 }
