788995d64d60e4a9fd14b651498f08cab4cd0fec
guilherme-marcello <guilemosm@gmail.com>
2023-06-25 18:12:55 +0100

    Added recursive totient function implementation and example of usage

--- 4-totient-function/totient_function.rs
+++ 4-totient-function/totient_function.rs
@@ -16,3 +16,22 @@ fn decompose_into_primes(n: u32) -> Vec<u32> {
 
     prime_factors
 }
+
+fn totient(n: u32) -> u32 {
+    let primes = decompose_into_primes(n);
+    println!("Prime factors of {}: {:?}", n, primes);
+    totient_aux(primes)
+}
+
+fn totient_aux(mut prime_factors: Vec<u32>) -> u32 {
+    match prime_factors.pop() {
+        Some(head) => (head -  1) * totient_aux(prime_factors),
+        None => 1,
+    }
+}
+
+fn main() {
+    let number = 4294967295;
+    let ttt = totient(number);
+    println!("Totient of {} is {}", number, ttt);
+}
